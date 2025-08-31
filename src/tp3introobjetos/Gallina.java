3e37a2fff19990f3aaf5e03449dbdea7a7e100fc
Tevan88 <tevanrivarola@gmail.com>
2025-08-30 23:13:01 -0300

    Del ejercicio 4

---
+++ src/tp3introobjetos/Gallina.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp3introobjetos;
+
+/**
+ *
+ * @author Esteban
+ */
+public class Gallina {
+    int edad;
+    int huevosPuestos;
+    int idGallina;
+    
+    void ponerHuevos () {
+        huevosPuestos ++;
+    }
+    
+    void envejecer () {
+        edad ++;
+    }
+    
+    void mostrarEstado(int idGallina){
+        System.out.println("La gallina " + idGallina + " ha puesto" + huevosPuestos + " y tiene " + edad + " anios");
+    }
+}
